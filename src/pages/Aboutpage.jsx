af2c0df8b2ae8bb3493a16ad5b14cf71ab8e350c
Lina Abuelezz <labuelez@uoguelph.ca>
2024-07-29 14:13:30 +0300

    adding links

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -16,11 +16,24 @@ const About = () => {
     }));
   };
 
+  const handleClubClick = (club) => {
+    if (club === 'SOCIS') {
+      window.open('https://clubs.socis.ca/', '_blank');
+    } else if (club === 'GCC') {
+      window.open('https://linktr.ee/socis?lt_utm_source=lt_share_link#332480509', '_blank'); 
+    } else if (club === 'GDSC') {
+      window.open('https://www.gdscguelph.com/', '_blank'); 
+    } else if (club === 'MSA') {
+      window.open('https://www.uogmsa.com/', '_blank'); 
+    }
+  };
+
+  const handleDarClick = () => {
+    window.open('https://www.bizmideast.com/EG/dar-al-fotouh-for-orphans-0114-404-4957', '_blank');
+  };
+
   const handleFootballClick = () => {
-    window.open(
-      'https://drive.google.com/file/d/1E4oGtnGj5aAp7r2lJrxpttCgaBEUFMyL/view?usp=sharing',
-      '_blank'
-    );
+    window.open('https://drive.google.com/file/d/1E4oGtnGj5aAp7r2lJrxpttCgaBEUFMyL/view?usp=sharing', '_blank');
   };
 
   return (
@@ -29,7 +42,7 @@ const About = () => {
         <div className="mb-8 p-4">
           <h1 className="text-4xl mb-4 relative group font-bold">
             About Me
-          <span className="absolute left-0 bottom-0 w-0 h-1 bg-black transition-all duration-500 group-hover:w-40"></span>
+            <span className="absolute left-0 bottom-0 w-0 h-1 bg-black transition-all duration-500 group-hover:w-40"></span>
           </h1>
           <p className="text-lg">
             Welcome to my About Me page! Here you'll find information about my education, involvement in clubs and organizations, and extracurricular activities. Click on the titles to expand each section and learn more about my experiences and interests.
@@ -42,11 +55,7 @@ const About = () => {
             onToggle={() => toggleSection('education')}
           >
             <p>
-              I was an IGCSE student in high school, so I took 3 A levels which
-              are considered university-level courses and one of the hardest
-              systems to be a part of. For university, I am attending the
-              University of Guelph, majoring in Computer Science and minoring in
-              Biology. Some relevant courses I've taken are:
+              I was an IGCSE student in high school, so I took 3 A levels which are considered university-level courses and one of the hardest systems to be a part of. For university, I am attending the University of Guelph, majoring in Computer Science and minoring in Biology. Some relevant courses I've taken are:
             </p>
             <ul className="list-disc list-inside ml-5">
               <li>CIS*2500 - Intermediate Programming</li>
@@ -64,11 +73,11 @@ const About = () => {
             onToggle={() => toggleSection('clubs')}
           >
             <ul className="list-disc list-inside ml-5">
-              <li>GCC (Guelph Coding Club) - Director of Communications</li>
-              <li>MIT (Muslims In Tech) - Software Developer</li>
-              <li>SOCIS (Society of Computing and Information Science) - Technology team member</li>
-              <li>GDSC (Guelph Developer Student Club) - Member</li>
-              <li>Guelph MSA (Muslim Student Association) - Member</li>
+              <li><span className="text-blue-500 hover:cursor-pointer" onClick={() => handleClubClick('GCC')}>GCC</span> (Guelph Coding Community) - Director of Communications</li>
+              <li><span className="text-blue-500 hover:cursor-pointer" onClick={() => handleClubClick('MIT')}>MIT</span> (Muslims In Tech) - Software Developer</li>
+              <li><span className="text-blue-500 hover:cursor-pointer" onClick={() => handleClubClick('SOCIS')}>SOCIS</span> (Society of Computing and Information Science) - Technology team member</li>
+              <li><span className="text-blue-500 hover:cursor-pointer" onClick={() => handleClubClick('GDSC')}>GDSC</span> (Guelph Developer Student Club) - Member</li>
+              <li><span className="text-blue-500 hover:cursor-pointer" onClick={() => handleClubClick('MSA')}>Guelph MSA</span> (Muslim Student Association) - Member</li>
               {/* Add more bullet points as needed */}
             </ul>
           </Section>
@@ -80,19 +89,11 @@ const About = () => {
             onToggle={() => toggleSection('extracurriculars')}
           >
             <p>
-              Currently, I play intramural soccer; however, I used to play soccer
-              semi-professionally, playing in the Egyptian Women's League. You can
-              see proof from the Egyptian Federation{' '}
-              <span
-                className="text-blue-500 hover:cursor-pointer"
-                onClick={handleFootballClick}
-              >
+              Currently, I play intramural soccer; however, I used to play soccer semi-professionally, playing in the Egyptian Women's League. You can see proof from the Egyptian Federation{' '}
+              <span className="text-blue-500 hover:cursor-pointer" onClick={handleFootballClick}>
                 here
               </span>
-              . Not only do I play guitar occasionally, but I also love
-              volunteering at various kinds of organizations to help out in any
-              way I can. One example is the time I volunteered at Dar al Fotooh,
-              which is an orphanage in Egypt.
+              . Not only do I play guitar occasionally, but I also love volunteering at various kinds of organizations to help out in any way I can. One example is the time I volunteered at <span className='text-blue-500 hover:cursor-pointer' onClick={handleDarClick}>Dar al Fotooh</span>, which is an orphanage in Egypt.
             </p>
           </Section>
         </Element>
